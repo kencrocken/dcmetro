4b8f9bf1f90df53a93aea16a29c43763fbb1e4a8
Ken Crocken <kcrocken@gmail.com>
2017-02-04 10:06:46 -0500

    adds test for lines and line

--- spec/test_spec.rb
+++ spec/test_spec.rb
@@ -24,10 +24,24 @@ describe DCMetro::Cli do
     end
 
     describe "#dcmetro station gallery -a" do
-        it "should echo predictions for Gallery Place" do
+        it "should echo predictions for Gallery Place with alerts" do
             echo_task = capture(:stdout) { DCMetro::Cli::Application.start(['station','gallery','-a']) }
             expect(echo_task).to include "***"
             expect(echo_task).to include "Gallery"
+        end
+    end
+
+    describe "#dcmetro lines" do
+        it "should echo metro lines" do
+            echo_task = capture(:stdout) { DCMetro::Cli::Application.start(['lines']) }
+            expect(echo_task).to eq "\e[0;94mBlue\e[0m\n\e[0;32mGreen\e[0m\n\e[38;5;208mOrange\e[0m\n\e[0;31mRed\e[0m\n\e[0;90mSilver\e[0m\n\e[0;93mYellow\e[0m\n"
+        end
+    end
+
+    describe "#dcmetro line" do
+        it "should echo metro line" do
+            echo_task = capture(:stdout) { DCMetro::Cli::Application.start(['line']) }
+            expect(echo_task).to eq "\e[0;94mBlue\e[0m\n\e[0;32mGreen\e[0m\n\e[38;5;208mOrange\e[0m\n\e[0;31mRed\e[0m\n\e[0;90mSilver\e[0m\n\e[0;93mYellow\e[0m\n"
         end
     end
 
